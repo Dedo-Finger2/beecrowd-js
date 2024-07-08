e1d3d10b1c204b93810e9d5d2dc90dc64a6a44e6
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-08 19:55:28 -0300

    chore: init

---
+++ src/challenges/1000.js
@@ -0,0 +1,11 @@
+/**
+ * Hello World!
+ * @returns { string }
+ */
+export function challenge1000() {
+  const message = "Hello World!";
+
+  console.log(message);
+
+  return message;
+};
---
+++ src/tests/1000.spec.js
@@ -0,0 +1,7 @@
+import { challenge1000 } from "../challenges/1000";
+
+describe("Hello World!", () => {
+  it("should return 'Hello World!'", () => {
+    expect(challenge1000()).toBe("Hello World!");
+  });
+});
